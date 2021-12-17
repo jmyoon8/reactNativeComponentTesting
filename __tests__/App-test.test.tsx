95b8c817c7232031c3a0dd6495bb3e4115626a38
yjj <jmyoon8@naver.com>
2021-12-17 10:38:36 +0900

    buttonPressTest

--- __tests__/App-test.test.tsx
+++ __tests__/App-test.test.tsx
@@ -21,7 +21,7 @@ describe("renders correctly", () => {
     // 스냅샷 : 해당 콤포넌트의 상태를 백업해놓는다(상태를 임시적으로 저장함)
     // 스냅샷을 찍게되면 테스파일 같은 레벨에 __snapshots__폴더안에 스냅샷 파일을 생성한다.
     // inline스냅샷 : 해당컴포넌트의 상태를 string화시켜서 자동으로 생성해준다(개꿀!)
-
+    // npm run test -u 으로 스탭샷을 다시찍을수있다.(초기화였던가.,.?)
     expect(renderd).toMatchSnapshot();
 
     // 일단 app.tsx컴포넌트에 대한 스냅샷을 찍어둔다.
--- __tests__/Profile.test.tsx
+++ __tests__/Profile.test.tsx
@@ -1,23 +1,31 @@
-import {render} from "@testing-library/react-native";
+import {fireEvent, render} from "@testing-library/react-native";
 import React from "react";
 import Profile from "../src/Profile";
 
 describe("ProfileComponent recived Props well?", () => {
-  test("is print UserName of props?", () => {
-    const willGiveProps = {
-      userName: "jammie",
-      name: "윤재진",
-    };
+  const willGiveProps = {
+    userName: "jammie",
+    name: "윤재진",
+  };
+  test("is print UserName and name of props?", () => {
     const rendered = render(<Profile {...willGiveProps} />);
     let userNameHasElement = rendered.getByText(
       `${willGiveProps.userName}(${willGiveProps.name})`,
     );
     // 어떤값이 있는지 확인
     console.log(userNameHasElement.props.children);
-    //  [ 'jammie', '(', '윤재진', ')' ]
-    expect(userNameHasElement.props.children[0]).toEqual(
-      willGiveProps.userName,
-    );
-    expect(userNameHasElement.props.children[2]).toEqual(willGiveProps.name);
+    // [ 'jammie', '(', '윤재진', ')' ]
+
+    expect(userNameHasElement.props.children).toContain(willGiveProps.name);
+    expect(userNameHasElement.props.children).toContain(willGiveProps.userName);
+  });
+  test("is onPress Button changed message?", () => {
+    const rendered = render(<Profile {...willGiveProps} />);
+    const byeButton = rendered.getByText("Bye!");
+    const helloButton = rendered.getByText("Hello!");
+    fireEvent(byeButton, "onPress");
+    rendered.getByText("Seeya!");
+    fireEvent(helloButton, "onPress");
+    rendered.getByText("Welcome!");
   });
 });
